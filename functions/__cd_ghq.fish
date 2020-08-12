465bb5695ca7b2e79454c4da4870aeee1b059632
USAMI Kosuke <usami.kosuke@gmail.com>
2020-08-12 13:11:02 +0900

    fzf sort enabled

--- functions/__cd_ghq.fish
+++ functions/__cd_ghq.fish
@@ -5,7 +5,7 @@ function __cd_ghq -d 'Change directory to select from ghq list'
     if not command -sq fzf
         echo 'Error: __cd_ghq needs fzf command.'
     end
-    ghq list --full-path | fzf --no-sort --query (commandline) | read selected
+    ghq list --full-path | fzf --query (commandline) | read selected
     test -n "$selected" ; and cd "$selected"
     commandline -f repaint
 end
