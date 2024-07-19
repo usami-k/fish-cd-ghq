d1f2bbb56fc7bf2686ac07c59cd1eb2d39305272
USAMI Kosuke <usami.kosuke@gmail.com>
2024-07-19 12:56:53 +0900

    Rename function

--- conf.d/cd_ghq.fish
+++ conf.d/cd_ghq.fish
@@ -1,4 +1,4 @@
-bind \cg '__cd_ghq'
+bind \cg 'cd_ghq'
 if bind -M insert >/dev/null
-    bind -M insert \cg '__cd_ghq'
+    bind -M insert \cg 'cd_ghq'
 end
--- functions/cd_ghq.fish
+++ functions/cd_ghq.fish
@@ -1,9 +1,9 @@
-function __cd_ghq -d 'Change directory to select from ghq list'
+function cd_ghq -d 'Change directory to select from ghq list'
     if not command -sq ghq
-        echo 'Error: __cd_ghq needs ghq command.'
+        echo 'Error: cd_ghq needs ghq command.'
     end
     if not command -sq fzf
-        echo 'Error: __cd_ghq needs fzf command.'
+        echo 'Error: cd_ghq needs fzf command.'
     end
     ghq list --full-path | fzf --query (commandline) | read selected
     test -n "$selected" ; and cd "$selected"
