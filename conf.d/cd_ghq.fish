aeec0cec56c1886eecce7f92267d28a505ac99ff
USAMI Kosuke <usami.kosuke@gmail.com>
2024-07-19 12:59:25 +0900

    Remove bindkey

--- conf.d/cd_ghq.fish
+++
@@ -1,4 +0,0 @@
-bind \cg 'cd_ghq'
-if bind -M insert >/dev/null
-    bind -M insert \cg 'cd_ghq'
-end
--- functions/cd_ghq.fish
+++ functions/cd_ghq.fish
@@ -1,11 +1,12 @@
-function cd_ghq -d 'Change directory to select from ghq list'
+function g -d 'Change directory to select from ghq list'
     if not command -sq ghq
         echo 'Error: cd_ghq needs ghq command.'
     end
     if not command -sq fzf
         echo 'Error: cd_ghq needs fzf command.'
     end
-    ghq list --full-path | fzf --query (commandline) | read selected
+    set query $argv[1]
+    ghq list --full-path | fzf --query "$query" | read selected
     test -n "$selected" ; and cd "$selected"
     commandline -f repaint
 end
